63e059d5a69dc42cc21330b6ce9f65ffa4cafc62
Mariam Ibrahim <mibrah7@students.towson.edu>
2018-11-04 15:57:57 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,60 @@
 def using_until
   levitation_force = 6
   #your code here
-    
+
 end
+
+def square_num(num)
+  num*num
+end
+puts square_num(square_num(10))
+
+counter=1 
+5.times do
+  puts square_num(counter)
+  counter=counter+1
+end 
+
+arr=[1, 2, 3, 4, 5]
+arr.length.times do
+ puts square_num(arr .shift)
+ end
+  
+index  
+while index < arr.length
+puts square_num(arr[index])
+index=index+1
+end
+
+while true 
+puts "hello"
+end
+
+index=0
+while index<5
+puts arr[index]
+index=index+1
+end
+
+
+
+
+names= [1, 2, 3, 4]
+def gen_pairs(arr)
+randomized=arr.shuffles
+counter=0
+while counter<randomized.length
+if counter+2<randomized.length
+puts "#{randomized[counter]} and #{randomized[counter+1]}"
+else
+counter=counter+2
+end
+
+
+
+  
+  
+  
+  
+  
 
